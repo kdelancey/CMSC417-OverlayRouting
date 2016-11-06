aeccf02c2cbbc4f61ce411ff0eb33938d2d01e0d
Raxce Eclar <reclar@terpmail.umd.edu>
2016-11-06 01:02:14 -0400

    Dumptable (#2)
    * Slight changes
    
    Cleaned up code a little. Added a Node object that will store all the
    information of that specific Node. Added correct commands to implement
    in file. Need to figure out what to store in Node objects and how to
    create routing table for each Node.
    
    * Added more code lol
    
    Added Utility class file that will hold all of our utilities. Currently
    Utility holds the methods to read in the configuration file and read in
    the nodes file.
    
    We just have to create the routing table and connect edges to the nodes
    now. The hard part haha.

--- node.rb
+++ node.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'utility'
 
 $port = nil
 $hostname = nil
@@ -7,14 +8,24 @@ $listenThread = nil
 
 # ----------------- PROJECT OBJECTS ---------------- # 
 
-class header
-	attr_accessor: hdr_sz #header size
-	attr_accessor: pyld_sz #payload size
-	attr_accessor: src_nd #source node
-	attr_accessor: dst_nd #destination node
-	attr_accessor: pkt_id #pkt id
-	attr_accessor: msg_lngth #source node
-	
+class Header
+	attr_accessor: hdr_sz 		# header size
+	attr_accessor: pyld_sz 		# payload size
+	attr_accessor: src_nd 		# source node
+	attr_accessor: dst_nd 		# destination node
+	attr_accessor: pkt_id 		# packet id
+	attr_accessor: msg_lngth 	# message length
+end
+
+class Node
+	attr_accessor: hostname		# name of node
+	attr_accessor: port				# port number this node should listen on
+	attr_accessor: nodes_map	# map of nodes and their ports
+	attr_accessor: update_int	# how often routing updates occur (secs)
+	attr_accessor: max_pyld		# maximum payload size for a message (bytes)
+	attr_accessor: timeout 		# how long it should wait for a reply (secs)
+	attr_accessor: node_time	# internal clock of node
+	attr_accessor: rt_table		# routing table for this node
 end
 
 
@@ -25,23 +36,24 @@ def edgeb(cmd)
 end
 
 def dumptable(cmd)
-	puts "DUMPTABLE: not implemented"
+	STDOUT.puts "DUMPTABLE: not implemented"
 end
 
 def shutdown(cmd)
-	STDOUT.puts "SHUTDOWN: not implemented"
+	STDOUT.flush
+	STDERR.flush
 	exit(0)
 end
 
 
-
 # --------------------- Part 1 --------------------- # 
+
 def edged(cmd)
 	STDOUT.puts "EDGED: not implemented"
 end
 
-def edgew(cmd)
-	STDOUT.puts "EDGEW: not implemented"
+def edgeu(cmd)
+	STDOUT.puts "EDGEU: not implemented"
 end
 
 def status()
@@ -50,6 +62,7 @@ end
 
 
 # --------------------- Part 2 --------------------- # 
+
 def sendmsg(cmd)
 	STDOUT.puts "SENDMSG: not implemented"
 end
@@ -67,11 +80,20 @@ def ftp(cmd)
 end
 
 
-def listenLoop()
+# --------------------- Part 3 --------------------- # 
 
-
-
+def circuitb(cmd)
+	STDOUT.puts "CIRCUITB: not implemented"
 end
+
+def circuitm(cmd)
+	STDOUT.puts "CIRCUITM: not implemented"
+end
+
+def circuitd(cmd)
+	STDOUT.puts "CIRCUITD: not implemented"
+end
+
 
 # Header Creator
 #
@@ -87,6 +109,7 @@ end
 #
 #
 #
+
 def header_parser( headerString )
 
 end
@@ -102,12 +125,16 @@ end
 #	from incoming connections, updating distance tables,
 #	<more tbd>
 
+def listenLoop()
+
+end
+
 # Command Loop
 #
 #	Loop that reads STDIN for input, and operates
 #	the given user command on this node.
-def command_loop()
 
+def commands()
 	while(line = STDIN.gets())
 		line = line.strip()
 		arr = line.split(' ')
@@ -116,7 +143,7 @@ def command_loop()
 		case cmd
 		when "EDGEB"; edgeb(args)
 		when "EDGED"; edged(args)
-		when "EDGEW"; edgew(args)
+		when "EDGEU"; edgew(args)
 		when "DUMPTABLE"; dumptable(args)
 		when "SHUTDOWN"; shutdown(args)
 		when "STATUS"; status()
@@ -124,26 +151,30 @@ def command_loop()
 		when "PING"; ping(args)
 		when "TRACEROUTE"; traceroute(args)
 		when "FTP"; ftp(args)
+		when "CIRCUITB"; circuitb(args)
+		when "CIRCUITM"; circuitm(args)
+		when "CIRCUITD"; circuitd(args)
 		else STDERR.puts "ERROR: INVALID COMMAND \"#{cmd}\""
 		end
 	end
-
 end
 
-def setup(hostname, port, nodes, config)
-	$hostname = hostname
-	$port = port
+def setup(hostnm, port, nodes, config)
+	hostname = Node.new
+	hostname.hostname = hostnm
+	hostname.port = port
+	hostname.nodes_map = Utility.read_nodes(nodes)
+
+	config_options = Utility.read_config(config)
+	hostname.update_int = config_options['updateInterval'].to_i
+	hostname.max_pyld = config_options['maxPayload'].to_i
+	hostname.timeout = config_options['pingTimeout'].to_i
 
 	server = TCPServer.new $port		
 
-	commandThread = Thread.new do
-		command_loop()
+	loop do
+		commands
 	end
-	
-	connectionListener = Thread.new do
-		
-		
-
 end
 
 setup(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
---
+++ utility.rb
@@ -0,0 +1,34 @@
+class Utility
+ 
+	# ====================================================================
+	# Reads in configuration file for node and returns a Hash containing
+	# the configuration options
+	# ====================================================================
+	def self.read_config(config_file)
+		config = Hash.new
+
+		File.open(config_file, 'r') do |file|
+			file.each_line do |line|
+				key, value = line.split('=')
+				config[key] = value
+			end
+		end
+		config
+	end
+
+	# ====================================================================
+	# Reads in nodes file and returns a Hash containing each node with 
+	# the corresponding port it should listen in on
+	# ====================================================================
+	def self.read_nodes(nodes_file)
+		nodes_map = Hash.new
+
+		File.open(nodes_file, 'r') do |file|
+			files.each_line do |line|
+				hostname, port = line.split(',')
+				nodes_map[hostname] = port
+			end
+		end
+		nodes_map
+	end
+end
