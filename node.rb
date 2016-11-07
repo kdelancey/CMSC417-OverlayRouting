ced8bab3d1f81b74e0dd469676d038e92644e04c
kdelancey <pandaplox@gmail.com>
2016-11-07 17:45:27 -0500

    Lots of additions in functions.
    Packet Class now fragments a passed message into an array of fragments, with properly configured headers and their section of the message
    Node.rb now has two threads, one for server-like operations, and another for client-like operations. The client connection thread acts on only operation for now, EDGEB. But later, this will sent packets by their array of fragments. The c thread holds a hash of nodenames to their socket connections, to properly address sending in the future. 
    The server thread is yet to be fully implemented. It needs to listen on its host port for incoming messages, and have those messages addressed somehow.
    Near no changes to Utilities, Raxce, consider making the port that you parse out straight an int, so I don't have to make it a to_i in the middle of my connection stuff.

--- node.rb
+++ node.rb
@@ -1,47 +1,48 @@
 require 'socket'
+require 'utility'
+require 'thread'
 
-$port = nil
-$hostname = nil
-$server = nil
-$listenThread = nil
+$port = nil				#this node's port number
+$hostname = nil			#this node's hostname
 
-# ----------------- PROJECT OBJECTS ---------------- # 
+$connectionMsgQueue = nil	#the queue of messages to the connectionThread
+$serverThread = nil		#the thread receiving incoming messages
+$connectionThread = nil	#the thread sending outgoing messages
+$nodes_map = nil		#hash of all nodes in nodes_file to their port numbers
 
-class header
-	attr_accessor: hdr_sz #header size
-	attr_accessor: pyld_sz #payload size
-	attr_accessor: src_nd #source node
-	attr_accessor: dst_nd #destination node
-	attr_accessor: pkt_id #pkt id
-	attr_accessor: msg_lngth #source node
-	
-end
+$config_options = nil	#array of all config options
+$update_int = nil		#how often routing updates should occur
+$max_pyld = nil			#the maximum size of information across one message
+$timeout = nil			#given timeout of ping in ms
 
+$node_time = nil		
+$rt_table = nil
 
 # --------------------- Part 0 --------------------- # 
 
-def edgeb(cmd)
+def edgeb(src_ip, dst_ip, dst)
 	STDOUT.puts "EDGE: not implemented"
 end
 
-def dumptable(cmd)
-	puts "DUMPTABLE: not implemented"
+def dumptable(filename)
+	Utility.dump_table(filename)
 end
 
-def shutdown(cmd)
-	STDOUT.puts "SHUTDOWN: not implemented"
+def shutdown()
+	STDOUT.flush
+	STDERR.flush
 	exit(0)
 end
 
 
-
 # --------------------- Part 1 --------------------- # 
+
 def edged(cmd)
 	STDOUT.puts "EDGED: not implemented"
 end
 
-def edgew(cmd)
-	STDOUT.puts "EDGEW: not implemented"
+def edgeu(cmd)
+	STDOUT.puts "EDGEU: not implemented"
 end
 
 def status()
@@ -50,6 +51,7 @@ end
 
 
 # --------------------- Part 2 --------------------- # 
+
 def sendmsg(cmd)
 	STDOUT.puts "SENDMSG: not implemented"
 end
@@ -67,83 +69,127 @@ def ftp(cmd)
 end
 
 
-def listenLoop()
+# --------------------- Part 3 --------------------- # 
 
-
-
+def circuitb(cmd)
+	STDOUT.puts "CIRCUITB: not implemented"
 end
 
-# Header Creator
-#
-#
-#
-
-# Packet Creator
-#
-#
-#
-
-# Header Parser
-#
-#
-#
-def header_parser( headerString )
-
+def circuitm(cmd)
+	STDOUT.puts "CIRCUITM: not implemented"
 end
 
-# Packet Parser
-#
-#
-#
+def circuitd(cmd)
+	STDOUT.puts "CIRCUITD: not implemented"
+end
 
-# Listener Loop
-#
-#	Listens on the current node's port, parsing headers
-#	from incoming connections, updating distance tables,
-#	<more tbd>
 
-# Command Loop
-#
-#	Loop that reads STDIN for input, and operates
-#	the given user command on this node.
-def command_loop()
-
+# ====================================================================
+# Reads STDIN for input and operates the given user command for this
+# node
+# ====================================================================
+def commands()
 	while(line = STDIN.gets())
 		line = line.strip()
 		arr = line.split(' ')
 		cmd = arr[0]
 		args = arr[1..-1]
 		case cmd
-		when "EDGEB"; edgeb(args)
+		when "EDGEB"; edgeb(arr[1], arr[2], arr[3])
 		when "EDGED"; edged(args)
-		when "EDGEW"; edgew(args)
-		when "DUMPTABLE"; dumptable(args)
-		when "SHUTDOWN"; shutdown(args)
+		when "EDGEU"; edgew(args)
+		when "DUMPTABLE"; dumptable(arr[1])
+		when "SHUTDOWN"; shutdown()
 		when "STATUS"; status()
 		when "SENDMSG"; sendmsg(args)
 		when "PING"; ping(args)
 		when "TRACEROUTE"; traceroute(args)
 		when "FTP"; ftp(args)
+		when "CIRCUITB"; circuitb(args)
+		when "CIRCUITM"; circuitm(args)
+		when "CIRCUITD"; circuitd(args)
 		else STDERR.puts "ERROR: INVALID COMMAND \"#{cmd}\""
 		end
 	end
-
 end
 
+# ====================================================================
+# Runs the node and sets up the configurations specified
+# ====================================================================
 def setup(hostname, port, nodes, config)
-	$hostname = hostname
-	$port = port
+	$hostname 		= hostname
+	$port 			= port
+	$nodes_map 		= Utility.read_nodes(nodes)
 
-	server = TCPServer.new $port		
-
-	commandThread = Thread.new do
-		command_loop()
-	end
+	$config_options = Utility.read_config(config)
+	$update_int 	= config_options['updateInterval'].to_i
+	$max_pyld 		= config_options['maxPayload'].to_i
+	$timeout 		= config_options['pingTimeout'].to_i
 	
-	connectionListener = Thread.new do
-		
-		
+	$connectionMsgQueue = Queue.new
 
+	$serverThread.new {
+		#open server listening on $port
+		socket = TCPServer.open( $port )
+		
+		#listen on port
+		while (true) 
+			Thread.start(socket.accept) do |client|
+				client.close
+			end
+		end
+	}	
+	
+	$connectionThread.new {
+	
+		#possibly place this elsewhere than setup, cuz it will get big
+		
+		#within thread, hold hash from destination to other nodes socket
+		nodeNameToSocketHash = Hash.new
+		
+		#constantly see if there is a message to pop on queue, if so...
+			#either EDGEB, which will create connection, and update the table
+			#within this thread, OR
+			#read the destination from parsing the packet to send message
+		
+		#connectionMsgQueue has message/command to be sent out. 
+		#threadMsg has message/command to be processed.
+		while (true)
+			threadMsg = nil
+			if (!connectionMsgQueue.empty?)
+				threadMsg = connectionMsgQueue.pop
+				
+				#LATER PLACED INTO ANOTHER METHOD for cleanliness
+				
+				#if message is EDGEB....
+				if ( threadMsg.include? "EDGEB" )
+					#Format: [EDGEB] [SRCIP] [DSTIP] [DST]
+					msgParsed = threadMsg.split(" ");
+					
+					#If the [DST] (destination node) given in EDGEB exists 
+					#	in nodes_map, then it is a valid node to connect with
+					
+					#Open a TCPSocket with the [DSTIP] (dest ip) on the given
+					#	portNum associated with DST on nodes_map
+					portNum = nodes_map[msgParsed[3]]
+					if ( nodes_map[msgParsed[3]] != nil )
+						nodeNameToSocketHash[msgParsed[3]] = \
+							TCPSocket.new msgParsed[2], portNum.to_i
+					end
+					
+				#elsif  ( verify it has valid header ) TODO
+				else #if message is packet....
+				 #do nothing for now
+				end
+				
+			end
+		end
+			
+	}
+	
+	loop do
+		commands
+	end
 end
 
 setup(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
